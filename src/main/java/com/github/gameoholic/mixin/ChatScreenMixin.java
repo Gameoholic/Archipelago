825c6130503c04495b6aa0686d91661b64058a98
Gameoholic <viner.atmic@gmail.com>
2023-10-22 01:56:38 +0300

    docs: add doc to mixin methods

--- src/main/java/com/github/gameoholic/mixin/ChatScreenMixin.java
+++ src/main/java/com/github/gameoholic/mixin/ChatScreenMixin.java
@@ -9,11 +9,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChatScreen.class)
 public class ChatScreenMixin {
+    /**
+     * Called when chat screen is opened
+     * @param info
+     */
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
         EmojiDialogUtil.INSTANCE.openDialog();
     }
 
+    /**
+     * Called when chat screen is closed
+     */
     @Inject(at = @At("HEAD"), method = "removed()V")
     private void removed(CallbackInfo info) {
         EmojiDialogUtil.INSTANCE.closeDialog();
